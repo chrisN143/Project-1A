5728db9b6105fc4ea5e3d1690a26a956985796ae
roysuryo <49808134+roysuryo@users.noreply.github.com>
2024-02-16 06:22:46 +0000

    perubahan pada nama

--- routes/web.php
+++ routes/web.php
@@ -75,22 +75,23 @@ Route::group(['middleware' => 'auth'], function () {
     });
 
     Route::prefix('payment')->name('payment.')->group(function () {
-        Route::controller(paymentontroller::class)->group(function () {
+        Route::controller(PaymentController::class)->group(function () {
             Route::get('/', 'index')->name('detail');
             Route::get('/detail', 'detail')->name('detail');
         });
     });
     Route::prefix('transaction')->name('transaction.')->group(function () {
-        Route::controller(transaction::class)->group(function () {
+        Route::controller(TransactionController::class)->group(function () {
             Route::get('/', 'index')->name('detail');
             Route::get('/detail', 'detail')->name('detail');
         });
     });
 
     Route::prefix('store')->name('store.')->group(function () {
-        Route::controller(store::class)->group(function () {
+        Route::controller(StoreController::class)->group(function () {
             Route::get('/', 'index')->name('detail');
             Route::get('/detail', 'detail')->name('detail');
         });
     });
+    
 });
