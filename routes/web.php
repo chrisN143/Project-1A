1fb230d43803e7f38bf62798e17669ddccabb5a7
roysuryo <49808134+roysuryo@users.noreply.github.com>
2024-02-16 06:15:52 +0000

    fungsi store

--- routes/web.php
+++ routes/web.php
@@ -75,13 +75,20 @@ Route::group(['middleware' => 'auth'], function () {
     });
 
     Route::prefix('payment')->name('payment.')->group(function () {
-        Route::controller(ProductController::class)->group(function () {
+        Route::controller(paymentontroller::class)->group(function () {
             Route::get('/', 'index')->name('detail');
             Route::get('/detail', 'detail')->name('detail');
         });
     });
     Route::prefix('transaction')->name('transaction.')->group(function () {
         Route::controller(transaction::class)->group(function () {
+            Route::get('/', 'index')->name('detail');
+            Route::get('/detail', 'detail')->name('detail');
+        });
+    });
+
+    Route::prefix('store')->name('store.')->group(function () {
+        Route::controller(store::class)->group(function () {
             Route::get('/', 'index')->name('detail');
             Route::get('/detail', 'detail')->name('detail');
         });
